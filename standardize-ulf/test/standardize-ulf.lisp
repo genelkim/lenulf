5e3352961ca0eac4689c17cb5f7445717336487c
Winnie Wan <winniewan6@gmail.com>
2021-03-25 15:33:04 -0400

    wrote 3 test functions for relational nouns

--- standardize-ulf/test/standardize-ulf.lisp
+++ standardize-ulf/test/standardize-ulf.lisp
@@ -38,3 +38,29 @@
     (not (null (possible-relative-clause? 
                  '(that.p ((past be.v) entertaining.a)))))))
 
+;; Winnie Wan, 2021-03-23
+;;
+;; Tests that check the relational noun erroneous patterns using 
+;; examples from FraCaS dataset.
+
+(define-test relational-noun-correction
+  (:tag :relational-nouns)
+
+  ; Tests function fix-relational-noun that takes erroneous relational noun sentence 
+  ; and returns the correct relational noun form.
+  (assert-true (equal '((THE.D (N+PREDS (PLUR (INHABITANT-OF.N (| CAMBRIDGE|))))) ((PAST VOTE.V) (FOR.P (A.D LABOURMP.N))))
+                        (fix-relational-noun '((THE.D (N+PREDS (PLUR INHABITANT.N) (OF.P | CAMBRIDGE|))) ((PAST VOTE.V) (FOR.P (A.D LABOURMP.N)))))))
+
+  ; Tests function convert-noun that takes in the erroneous relational noun sentence 
+  ; and the relational noun and returns (<noun-of> <term>) - combining noun + of + term.
+  (assert-true (equal '(chairman-of.n (the.d department.n))
+                        (convert-noun '(n+preds chairman.n (of.p (the.d department.n))) chairman.n)))
+
+  ; Tests function remove-n-preds that takes in the erroneous relational noun sentence 
+  ; and determines whether you remove n+preds depending on the number of predicates. 
+  ; If the sentence ahs 3 or more predicates, then remove n+preds = return t. Else, nil. 
+  (assert-true (equal t)
+                      (remove-n-preds '(n+preds chairman.n (of.p (the.d department.n))) chairman.n)))
+
+  (assert-true (equal nil)
+                      (remove-n-preds '((EVERY.D (CANADIAN.A RESIDENT.N)) ((PRES BE.V) (= (A.D (N+PREDS RESIDENT.N (OF.P (THE.D ((NORTH.A AMERICAN.A) CONTINENT.N))))))))
